be7adc637031beb4c16f7d42e12b894b52de8a41
Tatsunori Ono <tatsunori.no1@gmail.com>
2024-02-05 15:13:02 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
+import os
+...
+port = int(os.environ.get('PORT', 5000))
+...
+app.run(host='0.0.0.0', port=port, debug=True)
+
 from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
